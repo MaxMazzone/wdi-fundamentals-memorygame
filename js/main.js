9b20ebeb52a123b79694e8664402c83e4667842f
Max M <Max@Maxs-MacBook-Air.local>
2017-08-10 15:21:03 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var cards = [
 
 
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -32,21 +33,31 @@ var checkForMatch = function() {
 	}
 
 }
-var flipCard = function(cardId) {
-
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage)
 	console.log(cards[cardId].suit)
-	
+
+	this.setAttribute('src', cards[cardId].cardImage)
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 
-	
 }
-flipCard(3);
-flipCard(1);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png")
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
 
